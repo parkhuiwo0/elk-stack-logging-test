eb198ff80cbc90738cb16be490c79edf9086fc6f
HuiwooPark <huiwo0@naver.com>
2020-07-07 14:52:39 +0900

    init : 프로젝트 세팅

---
+++ src/main/java/com/parkhuiwo0/LogStashTest/LogStashTestApplication.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.LogStashTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogStashTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogStashTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkhuiwo0/LogStashTest/LogStashTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhuiwo0.LogStashTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogStashTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
